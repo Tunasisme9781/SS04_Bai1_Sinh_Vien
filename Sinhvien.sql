0c8e175ff5e7da516d24f8b883f5406160d8493a
Tunasisme9781 <ngoctuan186205@gmail.com>
2026-05-26 09:21:17 +0700

    Update Sinhvien.sql

--- Sinhvien.sql
+++ Sinhvien.sql
@@ -11,15 +11,33 @@ insert into students(name, age, major, gpa) values
                                                 ('Cường',22,'CNTT',3.8),
                                                 ('Dương',20,'Vật Lý',3.0),
                                                 ('Em',21,'CNTT',2.9);
+--1
 insert into students(name, age, major, gpa) values
     ('Hùng',23,'Hóa học',3.4);
-update students set gpa =3.6 where name = 'Bình';
-delete from students where gpa < 3.0;
-select name,major from students order by gpa desc ;
-select name from students where major = 'CNTT' limit 1;
-select * from students where gpa between 3.0 and 3.6;
-select * from students where name ilike 'C%';
-select *from students order by name asc limit 3;
-select *from students order by id asc limit 3 offset 1;
 
-select *from students
+--2
+update students set gpa =3.6 where name = 'Bình';
+
+--3
+delete from students where gpa < 3.0;
+
+--4
+select name,major from students order by gpa desc ;
+
+--5
+select name from students 
+where major = 'CNTT' order by id limit 1;
+
+--6
+select name,gpa from students 
+where gpa between 3.0 and 3.6;
+
+--7
+select name from students where name ilike 'C%';
+
+--8a
+select id,name from students order by name asc,id asc limit 3;
+
+--8b
+select id,name from students order by name asc,id asc limit 3 offset 1;
+
